3f28ffe3a1561dd8bf52595564d262f827e507e7
Miguel Akira Ykeuti Meiga <miguel.akira@gmail.com>
2012-03-14 14:03:01 -0300

    modif no tamanho do slider

--- slider.php
+++ slider.php
@@ -1,6 +1,6 @@
 <div class="slider-box">
  <div class="slider">
-    <div id="coin-slider"> <a href="#"><img src="images/slide1.jpg" width="800" height="400" alt="slide1" /></a> <a href="#"><img src="images/slide2.jpg" width="800" height="400" alt="slide2" /></a> <a href="#"><img src="images/slide3.jpg" width="800" height="400" alt="slide3" /></a> </div>
+    <div id="coin-slider"> <a href="#"><img src="images/slide1.jpg" width="950" height="400" alt="slide1" /></a> <a href="#"><img src="images/slide2.jpg" width="950" height="400" alt="slide2" /></a> <a href="#"><img src="images/slide3.jpg" width="950" height="400" alt="slide3" /></a> </div>
     <div class="clr"></div>
   </div>
   </div>
